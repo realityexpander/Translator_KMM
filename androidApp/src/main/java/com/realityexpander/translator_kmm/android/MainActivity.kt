7427a16f050e54e06baac842168d6b26b186c9cc
Chris Athanas <realityexpander@gmail.com>
2023-02-06 01:34:32 -0600

    rearrange code

--- androidApp/src/main/java/com/realityexpander/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/realityexpander/translator_kmm/android/MainActivity.kt
@@ -35,71 +35,6 @@ import com.realityexpander.translator_kmm.translate.presentation.TranslateState
 import com.realityexpander.translator_kmm.voice_to_text.presentation.VoiceToTextEvent
 import dagger.hilt.android.AndroidEntryPoint
 
-@Composable
-fun TranslatorTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        com.realityexpander.translator_kmm.android.core.theme.darkColors
-    } else {
-        com.realityexpander.translator_kmm.android.core.theme.lightColors
-    }
-    val SfProText = FontFamily(
-        Font(
-            resId = R.font.sf_pro_text_regular,
-            weight = FontWeight.Normal
-        ),
-        Font(
-            resId = R.font.sf_pro_text_medium,
-            weight = FontWeight.Medium
-        ),
-        Font(
-            resId = R.font.sf_pro_text_bold,
-            weight = FontWeight.Bold
-        ),
-    )
-    val typography = Typography(
-        h1 = TextStyle(
-            fontFamily = SfProText,
-            fontWeight = FontWeight.Bold,
-            fontSize = 30.sp
-        ),
-        h2 = TextStyle(
-            fontFamily = SfProText,
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp
-        ),
-        h3 = TextStyle(
-            fontFamily = SfProText,
-            fontWeight = FontWeight.Medium,
-            fontSize = 18.sp
-        ),
-        body1 = TextStyle(
-            fontFamily = SfProText,
-            fontWeight = FontWeight.Normal,
-            fontSize = 14.sp
-        ),
-        body2 = TextStyle(
-            fontFamily = SfProText,
-            fontWeight = FontWeight.Normal,
-            fontSize = 12.sp
-        ),
-    )
-    val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
-    )
-
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -195,5 +130,70 @@ fun TranslateRoot() {
             )
         }
     }
+}
+
+@Composable
+fun TranslatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        com.realityexpander.translator_kmm.android.core.theme.darkColors
+    } else {
+        com.realityexpander.translator_kmm.android.core.theme.lightColors
+    }
+    val SfProText = FontFamily(
+        Font(
+            resId = R.font.sf_pro_text_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.sf_pro_text_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.sf_pro_text_bold,
+            weight = FontWeight.Bold
+        ),
+    )
+    val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ),
+        body1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp
+        ),
+        body2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
+        ),
+    )
+    val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
 }
 
